047b4f4159f552b81e8e1173cc0d0825db309610
Garyismatic <garyevans1501@gmail.com>
2024-07-15 14:46:07 +0100

    created and tested get article by id endpoint

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -33,3 +33,30 @@ describe('/api/topics', () => {
         })
     })
 })
+describe('/api/articles/:article_id', () => {
+    describe('GET', () => {
+        it('returns an article corresponding to the article id given', () => {
+            return request(app)
+            .get('/api/articles/3')
+            .expect(200)
+            .then(({body}) => {
+                expect(body).toEqual({ article:
+                    {
+                    article_id: 3,
+                    title: "Eight pug gifs that remind me of mitch",
+                    topic: "mitch",
+                    author: "icellusedkars",
+                    body: "some gifs",
+                    created_at: expect.any(String),
+                    article_img_url:
+                      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+                      votes: 0
+                  }
+                })
+            })
+        })
+        describe('error handling for get /api/articles/article_id', () => {
+            
+        })
+    })
+})
--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { getTopics } = require('./controllers/topics.controllers')
 const { getEndpointList } = require('./controllers/endpoint-list.controller')
+const { getArticleById } = require('./controllers/articles.controller')
 
 
 const app = express()
 
 app.get('/api', getEndpointList)
 app.get('/api/topics', getTopics)
+app.get('/api/articles/:article_id', getArticleById)
 
 
 module.exports = app
