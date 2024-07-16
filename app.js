45c10b52f78e6aefa3e6f0448812c492b3d979f4
Garyismatic <garyevans1501@gmail.com>
2024-07-16 11:51:01 +0100

    created and tested retrieving comments by article id sorted in descending order

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { getTopics } = require('./controllers/topics.controllers')
 const { getEndpointList } = require('./controllers/endpoint-list.controller')
-const { getArticleById, getArticles } = require('./controllers/articles.controller')
+const { getArticleById, getArticles, getCommentsFromArticleId } = require('./controllers/articles.controller')
 const { customErrors, psqlCodeErrors } = require('./error-handlers')
 
 
@@ -11,6 +11,7 @@ app.get('/api', getEndpointList)
 app.get('/api/topics', getTopics)
 app.get('/api/articles', getArticles)
 app.get('/api/articles/:article_id', getArticleById)
+app.get('/api/articles/:article_id/comments', getCommentsFromArticleId)
 
 app.use(customErrors)
 app.use(psqlCodeErrors)
