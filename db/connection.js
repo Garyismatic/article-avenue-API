cffdbf6f09a9a5f3b13f9ed50125b89ce1371d3d
Garyismatic <garyevans1501@gmail.com>
2024-07-18 12:06:03 +0100

    update connection file

--- db/connection.js
+++ db/connection.js
@@ -9,11 +9,9 @@ require('dotenv').config({
 });
 
 if(ENV === 'production'){
-  config.connectionstring = process.env.DATABASE_URL
+  config.connectionString = process.env.DATABASE_URL
   config.max = 2
 }
-
-console.log(process.env)
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
