e0f790a2030cc16f76633f85504acdcff2a9be82
Garyismatic <garyevans1501@gmail.com>
2024-07-18 19:06:23 +0100

    created routers to handle middleware functions

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 const express = require('express')
-const { getTopics } = require('./controllers/topics.controller')
-const { getEndpointList } = require('./controllers/endpoint-list.controller')
-const { getArticleById, getArticles, getCommentsFromArticleId, postCommentToArticle, patchArticleVotes } = require('./controllers/articles.controller')
 const { customErrors, psqlCodeErrors } = require('./error-handlers')
-const { deleteComment } = require('./controllers/comments.controller')
-const { getUsers } = require('./controllers/users.controller')
+const  apiRouter  = require('./routers/api-router')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/api', getEndpointList)
-app.get('/api/topics', getTopics)
-app.get('/api/articles', getArticles)
-app.get('/api/articles/:article_id', getArticleById)
-app.get('/api/articles/:article_id/comments', getCommentsFromArticleId)
-app.get('/api/users', getUsers)
-
-app.post('/api/articles/:article_id/comments', postCommentToArticle)
-
-app.patch('/api/articles/:article_id', patchArticleVotes)
-
-app.delete('/api/comments/:comment_id', deleteComment)
+app.use('/api', apiRouter)
 
 app.use(customErrors)
 app.use(psqlCodeErrors)
