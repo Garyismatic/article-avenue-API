3fd13b5bfe5b87a031fef5e5ab19b078e3a9d454
Garyismatic <garyevans1501@gmail.com>
2024-07-18 22:13:08 +0100

    added the get user by username endpoint

--- models/comments.model.js
+++ models/comments.model.js
@@ -11,3 +11,13 @@ exports.removeComment = (commentId) => {
         }
     })   
 }
+
+exports.fetchUsername = (username) => {
+    return db.query('SELECT * FROM users WHERE username = $1', [username])
+    .then(({rows}) => {
+        if(rows.length === 0){
+            return Promise.reject({status: 404, message: 'not found'})
+        }
+        return rows[0]
+    })
+}
