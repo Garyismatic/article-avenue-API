b6cea652f2f5919c5c54174d4e28010256d8f029
Garyismatic <garyevans1501@gmail.com>
2024-08-05 09:49:49 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const { customErrors, psqlCodeErrors } = require('./error-handlers')
 const  apiRouter  = require('./routers/api-router')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
